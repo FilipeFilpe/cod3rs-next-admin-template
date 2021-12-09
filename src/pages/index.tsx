5f9b95304a48fbdb369e5aac39386e10af2c7bbd
filipefilpe <filpess@hotmail.com>
2021-12-09 15:26:52 -0300

    Initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import 'tailwindcss/tailwind.css'
+import Layout from '../components/template/Layout'
+
+export default function Home() {
+  
+  return (
+    <Layout titulo="Página Inicial" subtitulo="Estamos construíndo um template admin">
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
